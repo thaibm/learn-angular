796c72cb5fdb4785030dabe1806b5980ee969096
Bùi Minh Thái <thai.buiminh@nccsoft.office>
2019-07-10 18:10:17 +0700

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { WordComponent } from './components/word/word.component';
+import { BookComponent } from './components/book/book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WordComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
